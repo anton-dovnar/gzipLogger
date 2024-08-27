b42cb3fb2f710a5371e724fb551ba2cc7266b91f
Anton Dovnar <fode4cun@gmail.com>
2024-08-27 09:34:44 +0300

    [upd] Version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gziplogger',
-    version='0.1.1',
+    version='0.1.2',
     description='A logging utility package with gzip rotation and redirection of stdout/stderr',
     author='Anton Dovnar',
     author_email='anton.dovnar.tech@gmail.com',
